c6022c61ecf3df02effd1cbb39e61ce9375e3881
Abe <abesell132@gmail.com>
2022-08-30 21:56:36 -0400

    Updates

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://abesell:6TX38PNbZoG8zkAd@cluster0.a4arc.mongodb.net/boilerplateabesell?retryWrites=true&w=majority",
+  mongoURI: "",
   secretOrKey: "secret",
 };
